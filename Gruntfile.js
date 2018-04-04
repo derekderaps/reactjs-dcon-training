65346f91bb12af83d47e232f92c8ae8b504893dd
Hawkeye (Derek DeRaps) Tenderwolf <hawkeye@tenderwolf.io>
2018-04-04 16:05:42 -0400

    Initial commit, code from kalamuna/drupal-project

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  var pkg = require('./package.json');
+  var config = {
+    buildcontrol: {
+      options: {
+        dir: '.',
+        commit: true,
+        push: true,
+        shallowFetch: false,
+        fetchProgress: false,
+        force: true,
+        message: 'Built %sourceName% from commit %sourceCommit%',
+        connectCommits: false
+      },
+      deploy: {
+        options: {
+          remote: pkg.config.deploy.destination,
+          branch: process.env.CIRCLE_BRANCH || pkg.config.deploy.branch,
+          force: true
+        }
+      }
+    }
+  };
+
+  // Load Grunt config and the Build Control task.
+  grunt.initConfig(config);
+  grunt.loadNpmTasks('grunt-build-control');
+};
